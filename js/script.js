eead127ab17bda948ca6428433315e4cadef67a3
Beemo <louis.mornet@orange.fr>
2025-04-16 13:12:51 +0200

    fix light mode default

--- js/script.js
+++ js/script.js
@@ -1,84 +1,71 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+document.addEventListener('DOMContentLoaded', (event) => {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+  hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
 
-// Close navbar when link is clicked
-const navLink = document.querySelectorAll(".nav-link");
+  // Close navbar when link is clicked
+  const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
 
-// Event Listeners: Handling toggle event
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
+  // Event Listeners: Handling toggle event
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
+  toggleSwitch.addEventListener("change", switchTheme, false);
 
+  // Store color theme for future visits
+  function switchTheme(e) {
+    const theme = e.target.checked ? 'dark' : 'light';
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+    updateImage(theme);
+  }
 
-toggleSwitch.addEventListener("change", switchTheme, false);
+  // Save user preference on load
+  const currentTheme = localStorage.getItem("theme");
 
-//  Store color theme for future visits
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
-    updateImage('dark');
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    updateImage(currentTheme);
+    if (currentTheme === "dark") {
+      toggleSwitch.checked = true;
+    }
   } else {
     document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light"); //add this
+    localStorage.setItem("theme", "light");
     updateImage('light');
   }
-}
 
-// Save user preference on load
+  // Adding date
+  let myDate = document.querySelector("#datee");
+  const yes = new Date().getFullYear();
+  myDate.innerHTML = yes;
 
-const currentTheme = localStorage.getItem("theme")
-  ? localStorage.getItem("theme")
-  : null;
+  // Switch picture darkmode
+  function updateImage(theme) {
+    const profileImage = document.querySelector('.profile-image');
+    if (theme === 'dark') {
+      profileImage.src = 'assets/profile-image.png';
+    } else {
+      profileImage.src = 'assets/profile-image-black.png';
+    }
 
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-  updateImage(currentTheme)
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
+    const logoImage = document.querySelector('.logo-image');
+    if (theme === 'dark') {
+      logoImage.src = 'assets/logo.png';
+    } else {
+      logoImage.src = 'assets/logo-black.png';
+    }
   }
-}
-
-//Adding date
-
-let myDate = document.querySelector("#datee");
-
-const yes = new Date().getFullYear();
-myDate.innerHTML = yes;
-
-
-//switch picture darkmode
-
-function updateImage(theme) {
-  const profileImage = document.querySelector('.profile-image');
-  if (theme === 'dark') {
-    profileImage.src = 'assets/profile-image.png'; 
-  } else {
-    profileImage.src = 'assets/profile-image-black.png'; 
-  }
-
-  const logoImage = document.querySelector('.logo-image');
-  if (theme === 'dark') {
-    logoImage.src = 'assets/logo.png'; 
-  } else {
-    logoImage.src = 'assets/logo-black.png'; 
-  }
-  
-}
-
+});
